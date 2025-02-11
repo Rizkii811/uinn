b67c79799f0ffa8c9380fafd397a6c5278284718
Rizkii811 <rizkiefendi646@gmail.com>
2025-02-12 00:00:59 +0700

    Add files via upload

---
+++ TERBARU.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main (){
+	
+	// menampilkan tulisan hello word
+	cout<<"UIN adalah singkatan dari Universitas Islam Negeri, yaitu perguruan tinggi Islam negeri di Indonesia"<<endl;
+	cout<<"PAHAM KAN NYETTT";
+	
+	return 0;
+}
